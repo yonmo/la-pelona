7ad3817b33dd1173985d52273ba7d58c6505a4d0
yonmo <yonmo@idolum.io>
2022-10-29 20:30:21 +0000

    Fixed minor typos.

--- initialize.sh
+++ initialize.sh
@@ -2,10 +2,11 @@
 
 if [ "$EUID" = 0 ]; then
     echo "Initializing La Pelona Project App Image..."
+    sudo docker kill solid-chainsaw
     sudo docker system prune
     sudo docker build -t solid-chainsaw .
     sudo docker run -d -p 443:443 --name solid-chainsaw solid-chainsaw
 else
-    echo "Please Run As A Priveledged User..."
+    echo "Please Run As A Privileged User..."
     exit 1
 fi
